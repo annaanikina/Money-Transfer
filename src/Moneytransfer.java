c5ae5a15620db7be3d73c1f93899dae5bb8a644f
Anna Anikina <anna_anikina@mail.ru>
2021-07-16 00:58:05 +0300

    Initial commit

---
+++ src/Moneytransfer.java
@@ -0,0 +1,8 @@
+public class Moneytransfer {
+    public static void main(String[] args) {
+        int current_balance = 2_000_000_000;
+        int transfer_amount = 500_000_000;
+        int total_amount = current_balance + transfer_amount;
+        System.out.println(total_amount);
+    }
+}
